c656ec9e1770cf2d9781328458e8cb20398037ad
LucaTomala <luca.tomala@gmail.com>
2019-01-17 23:06:19 +0100

    hard work to run all but finally did it

--- deletable_primes.h
+++ deletable_primes.h
@@ -13,7 +13,9 @@
 #ifndef ___DELETABLE_PRIMES
 #define ___DELETABLE_PRIMES
 
-unsigned long remove_digit(int index, unsigned long n);
+unsigned long remove_digit(int i, unsigned long n);
+int get_ways(unsigned long n);
+int get_length(unsigned long n);
+bool is_prime(unsigned long n);
 
-int get_ways(unsigned long p);
 #endif
