2e6d4a4a12214a2257a10a47c2a4a0f20335b68b
LucaTomala <luca.tomala@gmail.com>
2019-01-17 11:33:42 +0100

    3 tests are running

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -10,3 +10,19 @@
  * Implementation of deletable_primes.h.
  * ----------------------------------------------------------
  */
+ #include "deletable_primes.h"
+ #include <math.h>
+ unsigned long remove_digit(int index, unsigned long n)
+ {
+   unsigned long digits_before=n/pow(10,index+1);
+ unsigned long digits_after=n%(unsigned long)pow(10,index);
+ unsigned long new_number=digits_before*pow(10,index)+digits_after;
+ return new_number;
+
+ }
+
+ int get_ways(unsigned long p)
+ {
+   return 0;
+
+ }
